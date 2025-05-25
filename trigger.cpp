e48ac741f771e6f1391f64129aae3e3651830915
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-05-25 00:06:59 -0300

    ISSO É MÁGIA NEGRA

--- trigger.cpp
+++ trigger.cpp
@@ -6,10 +6,14 @@ using namespace std;
 int main() {
     int i, j, limit;
 
-    limit = 2 << 7;
+    limit = pow(2,8);
 
     for (i = 0; i < limit; i++)
     {
       cout << dec << i << " : " << hex << i << endl;
+    }
+    for (i = limit; i < limit * 2; i++)
+    {
+      cout << dec << i << " : " << limit - 1 << endl;
     }
 }
