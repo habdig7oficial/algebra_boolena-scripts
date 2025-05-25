3e2b5ca83934ca77985e5928dc96a4e96d2a18fb
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-05-25 00:00:04 -0300

    ok

--- trigger.cpp
+++ trigger.cpp
@@ -1,27 +1,15 @@
 #include "iostream"
-
 #include "cmath"
 
 using namespace std;
 
 int main() {
+    int i, j, limit;
 
-  int limit = pow(2, 8);
-  for (int i = 0, j = 0; j < limit; ++i, ++j) {
+    limit = 2 << 7;
 
-    for(int k = 1, l = 0; k < limit; k = k << 0x4, l++){
-        if (((j % (0xa << (l * 0x4)) == 0))  && i != 0) {
-
-            i += (0x6 << (l * 0x4));
-
-
-        }
-        //cout << endl << "k: " << l << endl;
+    for (i = 0; i < limit; i++)
+    {
+      cout << dec << i << " : " << hex << i << endl;
     }
-
-
-    cout << hex << i << " : " << j << endl;
-  }
-
-  // cout << 0x160 - 0x99 << endl;
 }
