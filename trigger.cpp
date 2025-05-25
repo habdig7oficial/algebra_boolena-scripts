93192ecd0e08499ef5efd6331097b0d7eda0ef37
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2025-05-24 23:06:22 -0300

    staging

--- trigger.cpp
+++ trigger.cpp
@@ -1,14 +1,27 @@
 #include "iostream"
-#include "math.h"
+
+#include "cmath"
 
 using namespace std;
 
-int main(){
-    for(int i = 0, j = 0; i < pow(2,8); i++, j++){
-        if(j % 10 == 0 && i != 0){
-            i += 6;
-        }
+int main() {
 
-        cout << hex << i << " : " << dec << j << endl;
+  int limit = pow(2, 8);
+  for (int i = 0, j = 0; j < limit; ++i, ++j) {
+
+    for(int k = 1, l = 0; k < limit; k = k << 0x4, l++){
+        if (((j % (0xa << (l * 0x4)) == 0))  && i != 0) {
+
+            i += (0x6 << (l * 0x4));
+
+
+        }
+        //cout << endl << "k: " << l << endl;
     }
+
+
+    cout << hex << i << " : " << j << endl;
+  }
+
+  // cout << 0x160 - 0x99 << endl;
 }
